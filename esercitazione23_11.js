3a573da51a66aff1937eb8bf64725b75925504f8
Lorenzo_Cherchi <lorekk@htmail.it>
2023-11-23 22:06:40 +0100

    esercitazione23_11

---
+++ esercitazione23_11.js
@@ -0,0 +1,19 @@
+/* Creare un programma che offre una interfaccia di comando all’utente (clicca 1 fai questo,
+clicca 2 fai questo … etc … clicca 0 per chiudere)
+Il programma permette all’utente di creare una lista e offre queste funzionalità:
+
+-Aggiungi un elemento alla lista
+-Elimina un elemento dalla lista
+-Ordina la lista in ordine alfabetico
+-Modifica un elemento della lista
+-Elimina l’intera lista
+-Visualizza la lista
+
+Segui un approccio modulare, e crea tu stesso le rispettive funzioni per realizzare le funzionalità.
+Bilancia l’uso di funzioni preimpostate da quelle che crei tu stesso.
+
+Nota: rendi il programma reattivo, quindi ogni passo segue un feedback da parte dell’interfaccia,
+di corretta/incorretta esecuzione dell’operazione*/
+
+//  interfaccia user con switch case
+//
